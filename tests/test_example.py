fe78cd4557d78266babe328e88f5b458844b4427
Ignas Anikevicius <240938+aignas@users.noreply.github.com>
2024-06-29 15:28:32 +0900

    test: add more tests

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,2 +1,19 @@
-def test_foo():
-    assert 42 == 1
+from pathlib import Path
+import unittest
+
+
+def test_tmpdir(tmpdir):
+    tmp_file = Path(tmpdir) / "foo.txt"
+    want = "42"
+    tmp_file.write_text(want)
+    got = tmp_file.read_text()
+    assert want == got
+
+
+def test_failing():
+    assert False
+
+
+class TestCase(unittest.TestCase):
+    def test_simple(self):
+        self.assertEqual("42", "41")
