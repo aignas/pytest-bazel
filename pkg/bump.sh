093bba9345691d11d1417a9465baffbf2c5ab80a
Ignas Anikevicius <240938+aignas@users.noreply.github.com>
2024-07-05 18:20:34 +0900

    release: 0.0.5

--- pkg/bump.sh
+++ pkg/bump.sh
@@ -11,10 +11,10 @@ readonly ROOT="https://github.com/aignas/pytest-bazel"
 # * The currently unreleased should get the value.
 # * The extra text should be inserted at the top.
 # * The links at the bottom should be updated
-sed -i \
+sed \
     -e "s/## \[Unreleased\]/## [Unreleased\n\nNothing yet.\n\n## [$VERSION]/g" \
     -e "s|\[unreleased\]:.*|[unreleased]: $ROOT/compare/$VERSION...HEAD\n[$VERSION]: $ROOT/releases/tag/$VERSION|g" \
-    CHANGELOG.md
+    CHANGELOG.md >CHANGELOG.md
 
 # Update the version in the `pkg/BUILD.bazel`
 echo "VERSION = \"$VERSION\"" > ../version.bzl
